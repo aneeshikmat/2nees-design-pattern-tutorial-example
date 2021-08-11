36f48e70ac3d3333e2b571d88b2114ed98ebd5b9
aneeshikmat <aneeshikmat@gmail.com>
2021-08-11 06:07:02 +0300

    basic example 1 - end

--- behavioral-observer.php
+++ behavioral-observer.php
@@ -3,3 +3,108 @@
  * This example just to simulate how Observer can be work
  * 2nees.com
  */
+
+class Restaurant implements SplSubject {
+    private SplObjectStorage $observers;
+    private int $tableNumber = 0;
+    private int $numberOfCustomer = 0;
+
+    /**
+     * Restaurant constructor.
+     */
+    public function __construct()
+    {
+        $this->observers = new SplObjectStorage();
+    }
+
+    public function attach(SplObserver $observer)
+    {
+        $this->observers->attach($observer);
+    }
+
+    public function detach(SplObserver $observer)
+    {
+        $this->observers->detach($observer);
+    }
+
+    public function notify()
+    {
+        foreach ($this->observers as $observer) {
+            $observer->update($this);
+        }
+    }
+
+    /**
+     * Logic set here, we add notify if customer set down on table
+     */
+    public function customerSetOnTable(){
+        $this->numberOfCustomer = rand(1, 5);
+        $this->tableNumber += 1;
+
+        $this->notify();
+    }
+
+    public function getTableNumber(): int
+    {
+        return $this->tableNumber;
+    }
+
+    public function getNumberOfCustomer(): int
+    {
+        return $this->numberOfCustomer;
+    }
+}
+
+class Waiter implements SplObserver {
+    public function update(SplSubject $subject)
+    {
+        /**
+         * @var Restaurant $subject
+         */
+        echo "Hay!, We have a new customer here, check Table Number #{$subject->getTableNumber()}" . PHP_EOL;
+    }
+}
+
+class Kitchen implements SplObserver {
+    public function update(SplSubject $subject)
+    {
+        /**
+         * @var Restaurant $subject
+         */
+        echo "Hay!, We have an order for
+         Table Number #{$subject->getTableNumber()}
+         And We have {$subject->getNumberOfCustomer()} Customer" . PHP_EOL;
+    }
+}
+
+class Accountant implements SplObserver {
+    public function update(SplSubject $subject)
+    {
+        /**
+         * @var Restaurant $subject
+         */
+        $numberOfCustomer = $subject->getNumberOfCustomer();
+        $initPrice = $numberOfCustomer * 0.5;// Number of user * half JOD
+
+        echo "Hay!, Invoice Start for 
+         Table Number #{$subject->getTableNumber()}
+         And We have {$numberOfCustomer} Customer
+         And total init price is {$initPrice} JOD
+         " . PHP_EOL;
+    }
+}
+
+$restaurant = new Restaurant();
+$kitchen = new Kitchen();
+$waiter= new Waiter();
+$accountant = new Accountant();
+
+$restaurant->attach($waiter);
+$restaurant->attach($kitchen);
+$restaurant->attach($accountant);
+
+$restaurant->customerSetOnTable();
+echo "=======================================" . PHP_EOL;
+$restaurant->customerSetOnTable();
+echo "=======================================" . PHP_EOL;
+$restaurant->customerSetOnTable();
